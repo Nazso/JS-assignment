69be971c2b8c5d3b195f33b15e3476a2f859b3fc
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-03 20:57:30 +0200

    dev logic

--- main.js
+++ main.js
@@ -6,4 +6,36 @@ function computerPlay() {
     return choosedByComputer[numberOfChoosoing];
 }
 
-console.log(computerPlay())
+// console.log(computerPlay())
+
+function playGround (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+
+        return `You win, ${playerSelection} beats ${computerSelection}!`
+
+    }
+
+    else if (
+        (playerSelection === computerSelection)
+    ) {
+        return `You both chose ${playerSelection}`
+    }
+
+    else {
+        return "You lost the game!"
+    }
+}
+
+const playerSelection = "Paper";
+const computerSelection = computerPlay();
+
+console.log(computerSelection)
+console.log(playGround(playerSelection, computerSelection))
+
