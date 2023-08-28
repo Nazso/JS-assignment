482688db066eca466c012ca212eb70bc7e0740a4
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-28 23:54:26 +0200

    dev logic

--- JS/main_dom.js
+++ JS/main_dom.js
@@ -1,13 +1,16 @@
 const addName = document.querySelector("#addName");
 const saveName = document.querySelector(".saveName");
 const resetPage = document.querySelector(".reset");
+const playBtn = document.querySelector(".btn-play")
 
 const numberOfRounds = document.querySelector("#numberOfRounds")
+const computerChoose = document.querySelector(".computerChoose")
 
 const nameBox = document.querySelector(".nameBox");
 const text = document.querySelector(".text");
 
 let rounds = 5;
+const choosedByComputer = ["rock", "scissors", "paper"];
 
 function headName() {
     const nameText = document.createElement("p")
@@ -19,6 +22,22 @@ function headName() {
 resetPage.addEventListener('click', () => {
     location.reload()
 })
+
+function computerChoosen () {
+    const index = Math.floor(Math.random() * choosedByComputer.length);
+    return choosedByComputer[index];
+}
+
+playBtn.addEventListener("click", () => {
+    
+    const play = computerChoosen()
+    
+    const playText = document.createElement("p")
+
+    computerChoose.appendChild(playText)
+    playText.innerText = play
+    
+});
 
 saveName.addEventListener('click', () => {
 
